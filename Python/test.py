e91fa56e5b2b0eb786dd42561f756b03c3d7aeed
ChronoNewton <58590975+ChronoNewton@users.noreply.github.com>
2022-09-16 09:18:18 +0300

    some basic corrections

--- Python/test.py
+++ Python/test.py
@@ -3,5 +3,12 @@ from varFile import varFile
 test_varFile = varFile('../test.var')
 test_varFile.readVarFile()
 
-print(test_varFile.getValueByKey('name'))
-print(test_varFile.getValueByLineNumber(1))
+
+#variable value is retrieved by its key(name), which is e.g. 'NAME'
+print('My name is ' + test_varFile.getValueByKey('NAME'))
+
+#variable value is retrieved by the line number, which is e.g. 7
+print('My Ip is ' + test_varFile.getValueByLineNumber(7))
+
+#entire variable line is retrieved by the line number, which is e.g. 8
+print('\nfull variable line:\n' + test_varFile.getAbsLineAt(8))
